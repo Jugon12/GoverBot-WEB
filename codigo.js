066ae1b978d707bcd96ae76b7c437a8fc1c7e361
Daniel <daniel.vals.simon@gmail.com>
2024-04-13 22:29:26 +0200

    Comandos basicos bot-web

--- codigo.js
+++ codigo.js
@@ -1,8 +1,30 @@
 function pruebaDesdeWeb() {
     console.log("pito")
-    fetch('http://localhost:3000/llamar-funcion')
+    fetch('http://localhost:3000/enviar-mensaje')
         .then(response => response.text())
         .then(message => console.log(message))
         .catch(error => console.error('Error:', error));
     console.log("poto")
+}
+function pruebaDesdeWeb2() {
+    console.log("pito2")
+    
+    // const contenido = document.getElementById('contenidoTextarea').value;
+    const contenido = "Mensaje de prueba sacado de la web";
+    fetch('http://localhost:3000/enviar-contenido', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ contenido: contenido })
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Error al enviar contenido');
+        }
+        console.log('Contenido enviado correctamente');
+    })
+    .catch(error => console.error('Error:', error));
+    
+    console.log("poto2")
 }
